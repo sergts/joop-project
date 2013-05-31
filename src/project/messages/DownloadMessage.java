1a8316db9b8889634d2666bdbea2d2f796c99749
Sergei Tsimbalist <serjoz7@gmail.com>
2013-05-31 18:15:59 +0300

    who, checking ports

--- src/project/messages/DownloadMessage.java
+++ src/project/messages/DownloadMessage.java
@@ -1,5 +1,8 @@
 package project.messages;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
 import java.util.Iterator;
 
 
@@ -13,10 +16,15 @@ public class DownloadMessage extends Message {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	private static final int MAX_PORT_NUMBER = 65535;
+	private static final int DEFAULT_PORT = 8877;
+	private int port;
 
 	public DownloadMessage(String m, String to) {
 		super(m, to);
-		
+        port = DEFAULT_PORT;
 	}
 
 	@Override
@@ -28,27 +36,36 @@ public class DownloadMessage extends Message {
 	@Override
 	public void action(ClientSession sess) {
 
-		
+
 		Iterator<ClientSession> activeSessions = sess.getActiveSessions().iterator();
 		while (activeSessions.hasNext()) {
 			ClientSession session = activeSessions.next();
 			if (session.getName().equals(this.to)) {
-				
-				
+
+
 				for (String filename : session.files.keySet()) {
-					
+
 					if (filename.equalsIgnoreCase(this.contents)) {
                        
-						String send = session.files.get(filename).getPath() + " " + "8877";
+						while(true){
+							if(available(port)){
+								break;
+							}
+							else{
+								port = changePortValue(port);
+							}
+						}
 						
+						String send = session.files.get(filename).getPath() + " " + port;
+
 						session.sendMessage(new OpenUploadConnMsg(send));
-						
-						String receive = filename + " " + sess.ip + " " + "8877";
-						
+
+						String receive = filename + " " + sess.ip + " " + port;
+
 						sess.sendMessage(new OpenDownloadConnMsg(receive));
-						
-						
-						
+
+
+
 					}
 				}
 
@@ -56,5 +73,49 @@ public class DownloadMessage extends Message {
 		}
 
 	}
+	
+	
+	private int changePortValue(int currentPort) {
+		
+		if(currentPort==MAX_PORT_NUMBER){
+			return DEFAULT_PORT;
+		}
+		 return ++currentPort;
+		
+	}
+
+	private boolean available(int port) {
+	    if ( port > MAX_PORT_NUMBER) {
+	        throw new IllegalArgumentException("Invalid start port: " + port);
+	    }
+
+	    ServerSocket ss = null;
+	    DatagramSocket ds = null;
+	    try {
+	        ss = new ServerSocket(port);
+	        ss.setReuseAddress(true);
+	        ds = new DatagramSocket(port);
+	        ds.setReuseAddress(true);
+	        return true;
+	    } catch (IOException e) {
+	    } finally {
+	        if (ds != null) {
+	            ds.close();
+	        }
+
+	        if (ss != null) {
+	            try {
+	                ss.close();
+	            } catch (IOException e) {
+	                /* should not be thrown */
+	            }
+	        }
+	    }
+
+	    return false;
+	}
+	
+	
+	
 
 }
