536a00bb217d80db66f9ceb76ae6bed623039f4d
Sergei Tsimbalist <serjoz7@gmail.com>
2013-05-31 13:42:13 +0300

    share at start

--- src/project/client/Client.java
+++ src/project/client/Client.java
@@ -1,20 +1,12 @@
 package project.client;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-
-import java.net.*;
 import java.io.*;
 import java.util.*;
 
@@ -24,13 +16,13 @@ import project.messages.*;
 
 
 public class Client extends Thread {
-	
+
 	private static final int DOWNLOAD_QUERY_LENGTH = 4;
 	private static final int DOWNLOAD_INDEX = 0;
 	private static final int FILENAME_INDEX = 1;
 	private static final int FROM_INDEX = 2;
 	private static final int USERNAME_INDEX = 3;
-	
+
 	private DirWatcher watcher;
 	private String directory = "H:\\Projects\\test2";
 	private int port = 8888;
@@ -39,8 +31,8 @@ public class Client extends Thread {
 	private LinkedList<String> inQueue;
 	private ObjectInputStream netIn;
 	private ObjectOutputStream netOut;
-	
-	
+
+
 	public Client(int port, String dir){
 		this.port = port;
 		directory = dir;
@@ -49,19 +41,30 @@ public class Client extends Thread {
 	public Client(){
 		start();
 	}
-	
-	
-	
+
+
+
 	public void run(){
-		
+
 		//watcher = new DirWatcher(directory, this);
 		setInQueue(new LinkedList<String>()); 
 		setOut(new OutboundMessages());
 		
-		
+		String directory;
+		System.out.println("Enter \"SHARE\" <directory> ");
+		while(true){
+			directory = new Scanner(System.in).nextLine();
+			if(validateDirectory(directory)){
+				directory = directory.split(" ")[1];
+				break;
+			}
+			System.out.println("Invalid directory entered!");
+		}
+
+
 		System.out.println("Enter your name: ");
 		String myName = new Scanner(System.in).nextLine();
-		
+
 
 		try {
 			InetAddress servAddr = InetAddress.getByName("localhost");
@@ -76,34 +79,31 @@ public class Client extends Thread {
 			netOut = new ObjectOutputStream(getSocket().getOutputStream());
 			netOut.flush();
 			setNetIn(new ObjectInputStream(getSocket().getInputStream()));
-			
+
 			new ClientMessageSender(getOut(), netOut);
-			
+
 			BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
 			SocketListener l = new SocketListener(this);
-			
-			
+
+
 			//out.addMessage(new Message(IpChecker.getIp(), MessageType.QUERY));
 			out.addMessage(new InitIp(InetAddress.getLocalHost().getHostAddress()));
-			
+
 			out.addMessage(new InitName(myName));
-			
+
 			watcher = new DirWatcher(directory, this);
-			
+
 			String msg;
 
 			do { 
 				msg = stdin.readLine(); 
 				
-				if (msg.length() > 0) {
-					
-					parse(msg);
-						
-				}
+				parseMessage(msg);
 
-				if (!getInQueue().isEmpty()) { 
-					synchronized (getInQueue()) { 
+
+				if (!getInQueue().isEmpty()) { // kas on midagi saabunud?
+					synchronized (getInQueue()) { // lukku !!!
 						Iterator<String> incoming = getInQueue().iterator();
 						while (incoming.hasNext()) {
 							System.out.println(">> " + incoming.next());
@@ -122,12 +122,12 @@ public class Client extends Thread {
 			} catch (IOException e) {}
 		}
 	}
-	
+
 	public DirWatcher getWatcher(){
 		return watcher;
-		
+
 	}
-	
+
 	public void downloadConn(String ip, String fileName, int port){
 		new FileReceiver(fileName, ip, port);
 	}
@@ -135,35 +135,7 @@ public class Client extends Thread {
 		new FileSender(fileName, port);
 	}
 
-	public void parse(String msg){
-		
-		if(msg.equalsIgnoreCase("myfiles")){
-			System.out.println(watcher.getFiles());
-		}
-		else if(msg.equalsIgnoreCase("files")){
-			out.addMessage(new FilesQuery());
-		}
-		else if(msg.split(" ")[0].equalsIgnoreCase("share")){
-			File check = new File(msg.split(" ")[1]);
-			if (check.isDirectory()){
-				watcher.stopThread();
-				watcher = new DirWatcher(msg.split(" ")[1], this);
-			}
-		}
-		else if (msg.split(" ").length == DOWNLOAD_QUERY_LENGTH
-				&& msg.split(" ")[DOWNLOAD_INDEX].equalsIgnoreCase("download")
-				&& msg.split(" ")[FROM_INDEX].equalsIgnoreCase("from")) {
-			
-			out.addMessage(new DownloadMessage(
-					                   msg.split(" ")[FILENAME_INDEX],
-					                   msg.split(" ")[USERNAME_INDEX]));
 
-		}
-		
-		else{
-			out.addMessage(new TextMsg(msg));
-		}
-	}
 
 	public Socket getSocket() {
 		return socket;
@@ -210,5 +182,58 @@ public class Client extends Thread {
 	public void setOut(OutboundMessages out) {
 		this.out = out;
 	}
+	
+	public void parseMessage(String msg){
+		if (msg.length() > 0) {
+			
+			if(msg.equalsIgnoreCase("myfiles")){
+				System.out.println(watcher.getFiles());
+			}
+			else if(msg.equalsIgnoreCase("files")){
+				out.addMessage(new FilesQuery());
+			}
+			else if(validateDirectory(msg)){
+				
+					watcher.stopThread();
+					watcher = new DirWatcher(msg.split(" ")[1], this);
+				
+			}
+			else if (msg.split(" ").length == DOWNLOAD_QUERY_LENGTH
+					&& msg.split(" ")[DOWNLOAD_INDEX].equalsIgnoreCase("download")
+					&& msg.split(" ")[FROM_INDEX].equalsIgnoreCase("from")) {
+				
+				out.addMessage(new DownloadMessage(
+						msg.split(" ")[FILENAME_INDEX],
+						msg.split(" ")[USERNAME_INDEX]));
+				
+			}
+			
+			else{
+				out.addMessage(new TextMsg(msg));
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	private boolean validateDirectory(String directory){
+		
+		if(directory.split(" ")[0].equalsIgnoreCase("share")){
+			File check = new File(directory.split(" ")[1]);
+			if (check.isDirectory()){
+				check = null;
+				return true;
+			}
+			check = null;
+		}
+		
+		return false;
+		
+	}
+	
+	
 
+	
 }
